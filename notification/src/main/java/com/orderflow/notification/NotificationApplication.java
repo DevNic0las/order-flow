ec18eec2d2a2611b348e5a86d7b0bf75abdd6837
DevNic0las <nicolascajueiro098@gmail.com>
2026-09-02 13:21:33 -0300

    refactor(notification): remove redundant debug logging for Brevo properties in application startup

--- notification/src/main/java/com/orderflow/notification/NotificationApplication.java
+++ notification/src/main/java/com/orderflow/notification/NotificationApplication.java
@@ -10,13 +10,5 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication(scanBasePackages = "com.orderflow")
 @ConfigurationPropertiesScan
 public class NotificationApplication {
-
-  public static void main(String[] args) {
-    SpringApplication.run(NotificationApplication.class, args);
-  }
-  @Bean
-  public CommandLineRunner debugBrevo(BrevoProperties brevoProperties) {
-    return args -> System.out.println("BREVO BASE URL = [" + brevoProperties.apiKey() + "]");
-
-  }
+  public static void main(String[] args) {SpringApplication.run(NotificationApplication.class, args);}
 }
